07c401f2d60f39fb75e7f163c0bad3e811189cef
grdguez <36684964+grdguez@users.noreply.github.com>
2020-01-08 10:19:48 -0500

    Create name_old.py

---
+++ name_old.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+first_name = "xxx"
+last_name = "xxx"
+age = xx
+birth_date = "xx/xx/xxxx"
+
+print(f"My name is {first_name} {last_name}.")
+print(f"I was born on {birth_date}, and I'm {age} years old.")
